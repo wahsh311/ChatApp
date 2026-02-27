4eae716d3caa4fbf70c0df7d147095f27ae8ffcd
wahsh311 <wa7sh311@gmail.com>
2026-02-27 15:02:09 +0300

    added loadingView to signup

---
+++ ChatApp/Components/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  ChatApp
+//
+//  Created by Abdalqader Alwahsh on 27/02/2026.
+//
+
+import UIKit
+
+class LoadingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
